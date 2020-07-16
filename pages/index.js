13b621fde9fd755a2ccda1cd4555953d317f8b38
Jabir KH <jabir@datalkz.com>
2020-07-16 17:09:39 +0400

    :fire: refactored example code

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,20 @@ import { DTPageHeader, DTButton, DTDrawer, DTEmpty } from 'ui-antd-wrapper';
 import { useState } from 'react';
 
 export default function Home() {
+
   const [visible, setVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const loaderIllusion = () => {
+    if(!visible) {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    }
+    setVisible(!visible);
+  }
+
   return (
     <>
     <div className="container">
@@ -12,58 +25,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <DTPageHeader title="This is Header" subTitle="This is subtitle" />
+      <DTPageHeader title="AntD Wrapper Example" subTitle="This is the subtitle" />
 
       <main>
         <div>
           <DTButton 
             label="Click to open Drawer"
-            onClick={() => setVisible(!visible)}
+            onClick={loaderIllusion}
             type="primary"
             icon={{name : 'PlusOutlined'}}
           />
         </div>
       </main>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
     <DTDrawer
       title="Hello World"
       visible={visible}
       onClose={() => setVisible(false)}
+      placement="left"
+      loading={loading}
     >
       <DTEmpty description="Hello World" />
     </DTDrawer>
